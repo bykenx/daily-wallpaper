3fb3dc7b81ce3b2553b1e89dd8e88104b0e2ede1
byken <32236865+bykenx@users.noreply.github.com>
2021-08-08 18:50:10 +0800

    优化配置文件读写

--- go.mod
+++ go.mod
@@ -3,13 +3,15 @@ module daily-wallpaper
 go 1.15
 
 require (
+	github.com/anaskhan96/soup v1.2.4
 	github.com/andybrewer/mack v0.0.0-20200226161639-15be3d47cc54
 	github.com/getlantern/systray v1.1.0
 	github.com/gin-gonic/gin v1.7.2
 	github.com/google/go-querystring v1.1.0
+	github.com/imdario/mergo v0.3.12
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1
-	gopkg.in/yaml.v2 v2.2.8
+	gopkg.in/yaml.v2 v2.3.0
 	gorm.io/driver/sqlite v1.1.4
 	gorm.io/gorm v1.21.12
 )
--- settings.go
+++ settings.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/imdario/mergo"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 )
+
+var lastModifyTime int64
+var settings Settings
 
 func initSettings() {
 	if !IsDir(appHome) {
@@ -22,21 +26,25 @@ func initSettings() {
 		configBytes, _ := yaml.Marshal(settings)
 		_ = ioutil.WriteFile(p, configBytes, defaultFileCreatePermission)
 	}
+	settings = readSettings()
+	lastModifyTime = GetLastModifyTime(p)
 }
 
-
-
 func readSettings() Settings {
-	var settings Settings
-	configFilePath := filepath.Join(appHome, configFileName)
-	configBytes, _ := ioutil.ReadFile(configFilePath)
-	_ = yaml.Unmarshal(configBytes, &settings)
+	p := filepath.Join(appHome, configFileName)
+	if lastModifyTime < GetLastModifyTime(p) {
+		configFilePath := filepath.Join(appHome, configFileName)
+		configBytes, _ := ioutil.ReadFile(configFilePath)
+		_ = yaml.Unmarshal(configBytes, &settings)
+	}
 	return settings
 }
 
 func writeSettings(settings Settings) {
+	dst := readSettings()
+	_ = mergo.Merge(&dst, settings, mergo.WithOverride)
 	home, _ := os.UserHomeDir()
 	configFilePath := filepath.Join(home, fmt.Sprintf(".%s", packageName), configFileName)
-	configBytes, _ := yaml.Marshal(settings)
+	configBytes, _ := yaml.Marshal(dst)
 	_ = ioutil.WriteFile(configFilePath, configBytes, defaultFileCreatePermission)
 }
