7dea10f88bfce221e54e042756d2cda2fb9834cf
byken <32236865+bykenx@users.noreply.github.com>
2021-08-20 23:31:58 +0800

    完成壁纸自动切换，更新Readme

--- daily_wallpaper.go
+++ daily_wallpaper.go
@@ -18,6 +18,8 @@ func onExit() {
 
 func onReady() {
 
+	checkedChan := make(chan bool, 1)
+
 	InitSettings()
 	StartServer()
 
@@ -37,46 +39,49 @@ func onReady() {
 		WriteSettings(settings)
 	})
 
-	if settings.TimeToUpdate != nil && *settings.TimeToUpdate != "" {
-		task.RunAt(*settings.TimeToUpdate)
-	}
-	if settings.AutoUpdate != nil && *settings.AutoUpdate {
-		task.Start()
+	if *settings.AutoUpdate && *settings.TimeToUpdate != "" {
+		log.Printf("开启自动更新，更新时间设置为: %s\n", *settings.TimeToUpdate)
+		task.RunAt(*settings.TimeToUpdate).Start()
 	}
 
-	RegisterSettingsModifyCallback(func(settings Settings) {
-		if settings.CurrentImage != nil && *settings.CurrentImage != "" {
-			savedPath, err := downloadFileAndSave(*settings.CurrentImage)
-			if err != nil {
-				log.Printf("文件下载失败: %s\n", err)
-				return
+	RegisterSettingsModifyCallback(func(prev, current Settings) {
+		if prev.CurrentImage != current.CurrentImage && current.CurrentImage != nil {
+			if *current.CurrentImage != "" {
+				savedPath, err := downloadFileAndSave(*current.CurrentImage)
+				if err != nil {
+					log.Printf("文件下载失败: %s\n", err)
+					return
+				}
+				err = api.SetWallpaper(savedPath)
+				if err != nil {
+					log.Printf("设置壁纸失败: %s\n", err)
+					return
+				}
+				log.Println("切换壁纸成功")
 			}
-			err = api.SetWallpaper(savedPath)
-			if err != nil {
-				log.Printf("设置壁纸失败: %s\n", err)
-				return
+		}
+		if prev.TimeToUpdate != current.TimeToUpdate && current.TimeToUpdate != nil {
+			if *current.TimeToUpdate != "" {
+				log.Printf("更新时间设置为: %s", *current.TimeToUpdate)
+				task.RunAt(*current.TimeToUpdate)
 			}
-			log.Println("切换壁纸成功")
 		}
-		if settings.TimeToUpdate != nil && *settings.TimeToUpdate != "" {
-			log.Printf("自动切换壁纸时间设置为: %s", *settings.TimeToUpdate)
-			task.RunAt(*settings.TimeToUpdate)
-		}
-		if settings.AutoUpdate != nil {
-			if *settings.AutoUpdate {
+		if prev.AutoUpdate != current.AutoUpdate && current.AutoUpdate != nil {
+			if *current.AutoUpdate {
 				log.Println("开启自动更新")
+				checkedChan <- true
 				task.Restart()
 			} else {
 				log.Println("关闭自动更新")
+				checkedChan <- false
 				task.Stop()
 			}
 		}
 	})
 
-	fmt.Println("on Ready.")
 	systray.SetTitle("每日一图")
 	systray.SetIcon(icon.Data)
-	everydayItem := systray.AddMenuItemCheckbox("每日一图", "每日自动更新壁纸", true)
+	everydayItem := systray.AddMenuItemCheckbox("每日一图", "每日自动更新壁纸", *settings.AutoUpdate)
 	moreSettingItem := systray.AddMenuItem("更多设置", "更多设置")
 	quitItem := systray.AddMenuItem("退出", "退出应用程序")
 
@@ -98,6 +103,12 @@ func onReady() {
 				}
 			case <-moreSettingItem.ClickedCh:
 				OpenUrl("http://127.0.0.1:3000")
+			case v := <-checkedChan:
+				if v {
+					everydayItem.Check()
+				} else {
+					everydayItem.Uncheck()
+				}
 			}
 		}
 	}()
