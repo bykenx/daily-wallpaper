e94f59b899e09734e9df8ac6f3f243b97b52cade
byken <32236865+bykenx@users.noreply.github.com>
2026-05-23 15:36:49 +0800

    refactor: implement native wallpaper setting for macOS

--- api/set_wallpaper_darwin.go
+++ api/set_wallpaper_darwin.go
@@ -1,20 +1,47 @@
 package api
 
 import (
-	"fmt"
-
-	"github.com/andybrewer/mack"
+	"errors"
+	"unsafe"
 )
 
-func SetWallpaper(path string) error {
-	res, err := mack.Tell(
-		"Finder",
-		"activate",
-		fmt.Sprintf(`set desktop picture to POSIX file "%s"`, path),
-	)
-	if err != nil {
-		return err
+/*
+#cgo CFLAGS: -x objective-c
+#cgo LDFLAGS: -framework AppKit -framework Foundation
+#import <AppKit/AppKit.h>
+#import <Foundation/Foundation.h>
+#include <stdlib.h>
+
+char* setWallpaperForAllScreens(const char* path) {
+	@autoreleasepool {
+		NSString *pathString = [NSString stringWithUTF8String:path];
+		NSURL *imageURL = [NSURL fileURLWithPath:pathString];
+		NSArray<NSScreen *> *screens = [NSScreen screens];
+		NSWorkspace *workspace = [NSWorkspace sharedWorkspace];
+
+		for (NSScreen *screen in screens) {
+			NSError *error = nil;
+			BOOL ok = [workspace setDesktopImageURL:imageURL forScreen:screen options:@{} error:&error];
+			if (!ok) {
+				NSString *message = error ? [error localizedDescription] : @"设置壁纸失败";
+				return strdup([message UTF8String]);
+			}
+		}
+
+		return NULL;
 	}
-	println(res)
+}
+*/
+import "C"
+
+func SetWallpaper(path string) error {
+	cPath := C.CString(path)
+	defer C.free(unsafe.Pointer(cPath))
+
+	errMsg := C.setWallpaperForAllScreens(cPath)
+	if errMsg != nil {
+		defer C.free(unsafe.Pointer(errMsg))
+		return errors.New(C.GoString(errMsg))
+	}
 	return nil
 }
